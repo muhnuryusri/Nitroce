c500862074fb1811d550767391e3c6a57c3130dd
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-07 23:48:11 +0800

    add utils

---
+++ app/src/main/java/com/example/nitroce/utils/FileItem.kt
@@ -0,0 +1,8 @@
+package com.example.nitroce.utils
+
+data class FileItem(
+    var path: String? = null,
+    var size: String? = null,
+    var name: String? = null,
+    var confidence_score: Double = 0.0
+)
